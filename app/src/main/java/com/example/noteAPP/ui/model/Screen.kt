2ea2cea05684d1509b521430b37662ad9138b463
Sternerr <volpy2000@gmail.com>
2024-11-12 20:14:01 +0100

    Screen Model

--- app/src/main/java/com/example/noteAPP/ui/model/Screen.kt
+++ app/src/main/java/com/example/noteAPP/ui/model/Screen.kt
@@ -3,6 +3,10 @@ package com.example.noteAPP.ui.model
 sealed class Screen(val route: String) {
     //Object define and create an instance of a class in a single step
     object Overview : Screen("overview")
-    object NoteDetail : Screen("note-detail")
-    object NoteEdit : Screen("note-edit")
+    object NoteEdit : Screen("note-edit") {
+        fun getNote(noteId: Int) = "$route/$noteId"
+    }
+    object NoteDetail : Screen("note-detail") {
+        fun getNote(noteId: Int) = "$route/$noteId"
+    }
 }
