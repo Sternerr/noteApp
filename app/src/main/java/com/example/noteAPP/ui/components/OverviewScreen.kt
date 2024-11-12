d68c4dda7824d8fe9c1f2fc0ee488b89ebcf18cc
Sternerr <volpy2000@gmail.com>
2024-11-12 20:17:03 +0100

    OverviewScreen Component

--- app/src/main/java/com/example/noteAPP/ui/components/OverviewScreen.kt
+++ app/src/main/java/com/example/noteAPP/ui/components/OverviewScreen.kt
@@ -1,17 +1,8 @@
 package com.example.todoapp.ui.components
 
 import android.annotation.SuppressLint
-import android.app.LauncherActivity
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -23,28 +14,17 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.noteAPP.ui.components.SwipeToDeleteContainer
-import com.example.noteAPP.ui.model.Note
+import com.example.noteAPP.ui.components.SwipeContainer
+import com.example.noteAPP.ui.model.Screen
 import com.example.todoapp.ui.repository.NoteRepository
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -80,20 +60,31 @@ fun OverviewScreen(noteRepository: NoteRepository, navController: NavController)
                             it.id
                         }
                     ) { note ->
-                        SwipeToDeleteContainer(
+                        SwipeContainer(
                             note = note,
                             onDelete = {
                                 noteRepository.removeNote(note)
-                                Log.d("Remove Note", "${noteRepository.noteList}")
                             },
+                            onModify = {
+                                navController.navigate(
+                                    Screen.NoteEdit.getNote(note.id)
+                                )
+                            }
                         ) {
                             ListItem(
                                 headlineContent = { Text(it.title) },
-                                supportingContent = { Text(it.text) },
+                                supportingContent = {
+                                    Text( text = it.text )
+                                },
                                 colors = ListItemDefaults.colors(Color.LightGray),
                                 modifier = Modifier
                                     .padding(16.dp)
-                                    .clip(RoundedCornerShape(16.dp)),
+                                    .clip(RoundedCornerShape(16.dp))
+                                    .clickable {
+                                        navController.navigate(
+                                            Screen.NoteDetail.getNote(note.id)
+                                        )
+                                    }
                             )
                         }
                     }
